3b3210f5be3cb81c148c0d8462fd34cd265db05f
Ian Wagner <ian.wagner@stadiamaps.com>
2023-06-26 15:40:13 +0900

    Improve documentation of ranged elevation responses

--- __tests__/geospatial.test.ts
+++ __tests__/geospatial.test.ts
@@ -5,6 +5,7 @@ import {disableFetchMocks, enableFetchMocks} from "jest-fetch-mock";
 // Fixtures captured from the API
 const seoul = {lat: 37.56, lng: 126.99};
 const heightReq = {id: "Seoul", shape: [{lat: seoul.lat, lon: seoul.lng}]};
+const rangedHeightReq = {range: true, ...heightReq};
 const mockTzRes = {
     "tz_id": "Asia/Seoul",
     "base_utc_offset": 32400,
@@ -81,6 +82,15 @@ describe('GeospatialApi unit tests', () => {
         expect(res.id).toEqual(heightReq.id);
         expect(res.height?.length).toBeGreaterThanOrEqual(1);
         expect(res.height![0]).toBeGreaterThanOrEqual(0);
+        expect(res.shape).toEqual(heightReq.shape);
+    })
+
+    test('elevation endpoint integration test (Seoul), with height ranges', async () => {
+        const res = await api.elevation({heightRequest: rangedHeightReq});
+        expect(res.id).toEqual(heightReq.id);
+        expect(res.rangeHeight?.length).toBeGreaterThanOrEqual(1);
+        expect(res.rangeHeight![0][0]).toEqual(0);  // Always null for the first height
+        expect(res.rangeHeight![0][1]).toBeGreaterThanOrEqual(0);
         expect(res.shape).toEqual(heightReq.shape);
     })
 });
